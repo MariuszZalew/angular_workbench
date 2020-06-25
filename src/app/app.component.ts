2d6c63c0d7a0fe4ce228c5013cb3cfaf1d4ebc0b
Mariusz <m.zalew.poczta@gmail.com>
2020-06-25 11:39:15 +0200

    form is now working properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,13 +20,13 @@ export class AppComponent {
     ]
   }
 
+  
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    this.allowButton = !this.allowButton;
-
-    setTimeout(() => {
-      this.allowButton = !this.allowButton;
-    }, 800);
+    console.log(`Adding article title ${title.value} and link ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
+  
 }
