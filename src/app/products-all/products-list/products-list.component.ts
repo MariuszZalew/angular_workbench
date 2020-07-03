9ff2cd431ba7903319e2a6daa65aa7a1795a82d5
Mariusz <m.zalew.poczta@gmail.com>
2020-07-03 14:08:27 +0200

    rm unnecessary code

--- src/app/products-all/products-list/products-list.component.ts
+++ src/app/products-all/products-list/products-list.component.ts
@@ -6,30 +6,15 @@ import {
 } from '@angular/core';
 import { Product } from '../product.model';
 
-/**
- * @ProductsList: A component for rendering all ProductRows and
- * storing the currently selected Product
- */
 @Component({
   selector: 'products-list',
   templateUrl: './products-list.component.html'
 })
 export class ProductsListComponent {
-  /**
-   * @input productList - the Product[] passed to us
-   */
   @Input() productList: Product[];
 
-  /**
-   * @output onProductSelected - outputs the current
-   *          Product whenever a new Product is selected
-   */
   @Output() onProductSelected: EventEmitter<Product>;
 
-  /**
-   * @property currentProduct - local state containing
-   *             the currently selected `Product`
-   */
   private currentProduct: Product;
 
   constructor() {
