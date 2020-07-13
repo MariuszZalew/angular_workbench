216e53cd7600e44713b47d842b9784af67afc0b7
Mariusz <m.zalew.poczta@gmail.com>
2020-07-13 13:58:30 +0200

    Add routing & other minor stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from "./app.component";
-import { ProductImageComponent } from "./products-all/product-image/product-image.component";
-import { ProductDepartmentComponent } from "./products-all/product-department/product-department.component";
-import { PriceDisplayComponent } from "./products-all/price-display/price-display.component";
-import { ProductRowComponent } from "./products-all/product-row/product-row.component";
-import { ProductsListComponent } from "./products-all/products-list/products-list.component";
-import { ProductsAllComponent } from "./products-all/products-all.component";
-import { HelloWorldComponent } from "./hello-world/hello-world.component";
-import { ExerciseOneComponent } from "./exercise-one/exercise-one.component";
-import { ExerciseTwoComponent } from "./exercise-two/exercise-two.component";
+import { AppComponent } from './app.component';
+import { ProductImageComponent } from './products-all/product-image/product-image.component';
+import { ProductDepartmentComponent } from './products-all/product-department/product-department.component';
+import { PriceDisplayComponent } from './products-all/price-display/price-display.component';
+import { ProductRowComponent } from './products-all/product-row/product-row.component';
+import { ProductsListComponent } from './products-all/products-list/products-list.component';
+import { ProductsAllComponent } from './products-all/products-all.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { ExerciseOneComponent } from './exercise-one/exercise-one.component';
+import { ExerciseTwoComponent } from './exercise-two/exercise-two.component';
 import { VoteDomainComponent } from './vote-domain/vote-domain.component';
 import { ArticleComponent } from './vote-domain/article/article.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+
+const appRoutes: Routes = [
+  { path: 'Exercise1', component: ExerciseOneComponent },
+  { path: 'Exercise2', component: ExerciseTwoComponent },
+  { path: 'Hello', component: HelloWorldComponent },
+  { path: 'Vote', component: VoteDomainComponent },
+  { path: 'Products', component: ProductsAllComponent }
+];
 
 @NgModule({
   declarations: [
@@ -30,10 +39,12 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
     ExerciseTwoComponent,
     VoteDomainComponent,
     ArticleComponent,
-    NavMenuComponent
+    NavMenuComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule,
+            FormsModule, 
+            RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
