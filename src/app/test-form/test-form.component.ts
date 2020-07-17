992ef5c2c59d854a1f4068b807890bfd4facbbe2
Mariusz <m.zalew.poczta@gmail.com>
2020-07-17 13:09:05 +0200

    use ngForm to view result of simple function

--- src/app/test-form/test-form.component.ts
+++ src/app/test-form/test-form.component.ts
@@ -10,7 +10,15 @@ export class TestFormComponent implements OnInit {
 
   @ViewChild('bar') singUp: NgForm; 
 
-  constructor() { }
+  testString: string = 'Please, input any string';
+  hasBeenEvaluated: boolean = false;
+  reverseResultView: string;
+
+  constructor() {
+    if(this.testString !== '') {
+      this.testString = '';
+    }
+  }
 
   ngOnInit(): void {
   }
@@ -22,5 +30,21 @@ export class TestFormComponent implements OnInit {
   onSubmitTwo() {
     console.log(this.singUp.value);
   }
+
+  onSubmitReverseStr(f: NgForm) {
+    console.log(f.value['revStr']);
+    console.log(this.reverseOne(f.value['revStr']));
+    this.reverseResultView = this.reverseOne(f.value['revStr']);
+    this.hasBeenEvaluated = true;
+    this.testString = '';
+  }
+
+  reverseOne(str: string) {
+    let resultString: string = '';
+    for(let i = str.length; i >= 0 ; i--) {
+        resultString += str.charAt(i);
+    }
+    return resultString;
+}
 
 }
