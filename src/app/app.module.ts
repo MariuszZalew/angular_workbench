f2d6623f9553bc7f3ccad92615bab368ef9ba4bc
Mariusz <m.zalew.poczta@gmail.com>
2020-07-24 13:51:41 +0200

    rm unneeded import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { GroupFormComponent } from './main-form/group-form/group-form.component'
 import { StringReverseFormComponent } from './main-form/string-reverse-form/string-reverse-form.component';
 import { MaxCharFormComponent } from './main-form/max-char-form/max-char-form.component';
 import { IntReverseFormComponent } from './main-form/int-reverse-form/int-reverse-form.component';
-import { RevStringService } from './main-form/rev-string.service';
+// import { RevStringService } from './main-form/rev-string.service';
 
 const appRoutes: Routes = [
   { path: 'Vote', component: VoteDomainComponent },
@@ -44,7 +44,7 @@ const appRoutes: Routes = [
             FormsModule, 
             ReactiveFormsModule,
             RouterModule.forRoot(appRoutes)],
-  providers: [RevStringService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
